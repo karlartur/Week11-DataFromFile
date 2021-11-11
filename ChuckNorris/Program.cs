7b08944db7e858d48e607c999100d18b7b8759ec
karlartur <89841872+karlartur@users.noreply.github.com>
2021-11-11 10:24:42 +0200

    Add files via upload

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckNorris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(RandomLineFromFile());
+            }
+        }
+        private static string RandomLineFromFile()
+        {
+            
+            string filePath = @"C:\Users\opilane\Sample\chuck.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, dataFromFile.Length);
+            return dataFromFile[randomIndex];
+        }
+        
+    }
+}
